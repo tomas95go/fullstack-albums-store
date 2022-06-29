6b8b5981f59c0bfe153b797b82ffcd6f32c42bd7
tomas95go <tomas95go@gmail.com>
2022-06-29 19:24:09 -0300

    Super basic form

--- client/src/routes/form.albums.jsx
+++ client/src/routes/form.albums.jsx
@@ -1,7 +1,62 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+
 export default function FormAlbum() {
+  const sayHi = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const name = form.get("album-name");
+    const artist = form.get("album-artist");
+    const price = form.get("album-price");
+    const cover = form.get("album-cover");
+    const releaseYear = form.get("album-release-year");
+
+    const newAlbum = {
+      name,
+      artist,
+      price,
+      cover,
+      releaseYear,
+    };
+  };
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>AlbumForm</h2>
-    </main>
+    <Flex w="100%" align="center" justify="center">
+      <Box as="form" w="50%" onSubmit={sayHi}>
+        <FormControl py={2}>
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input id="name" type="text" name="album-name" />
+        </FormControl>
+        <FormControl py={2}>
+          <FormLabel htmlFor="artist">Artist</FormLabel>
+          <Input id="artist" type="text" name="album-artist" />
+        </FormControl>
+        <FormControl py={2}>
+          <FormLabel htmlFor="price">Price</FormLabel>
+          <Input id="price" type="number" name="album-price" />
+        </FormControl>
+        <FormControl py={2}>
+          <FormLabel htmlFor="cover">Cover (URL IMG)</FormLabel>
+          <Input id="cover" type="text" name="album-cover" />
+        </FormControl>
+        <FormControl py={2}>
+          <FormLabel htmlFor="release-year">Release Year</FormLabel>
+          <Input id="release-year" type="number" name="album-release-year" />
+        </FormControl>
+        <FormControl py={2}>
+          <Flex justify="center">
+            <Button colorScheme="blue" type="submit">
+              Submit
+            </Button>
+          </Flex>
+        </FormControl>
+      </Box>
+    </Flex>
   );
 }
