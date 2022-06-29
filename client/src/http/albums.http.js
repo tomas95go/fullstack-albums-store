62ba592be9e326db5dcf2cd00697871f438f5e8a
tomas95go <tomas95go@gmail.com>
2022-06-29 20:54:10 -0300

    Basic data post from client to server

--- client/src/http/albums.http.js
+++ client/src/http/albums.http.js
@@ -1,5 +1,15 @@
-const httpPostNewAlbum = (newAlbum) => {
-  console.log(newAlbum);
+const API_URL = `http://localhost:8000`;
+
+const httpPostNewAlbum = async (newAlbum) => {
+  const response = await fetch(`${API_URL}/albums`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(newAlbum),
+  });
+  const album = await response.json();
+  console.log(album);
 };
 
 export { httpPostNewAlbum };
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("Hello world!");
+});
+
+app.post("/albums", (request, response) => {
+  const album = request.body;
+  response.status(200).json({
+    message: "New album successfully created",
+    album,
+  });
 });
 
 module.exports = app;
